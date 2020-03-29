3dec1322fcca229d50ac184a783ab402c9b17cc3
MurungaKibaara <murungakibaara@outlook.com>
2020-03-30 00:38:03 +0300

    [Expo Redux setup]

---
+++ README.md
@@ -0,0 +1,10 @@
+### expo-redux boilerplate
+Expo, Redux and Navigation BoilerPlate to get your react native project started.
+
+### Expo 36 SDK.
+
+Git clone the repository.
+
+- yarn install
+- yarn start
+- expo start
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Store from './Store'
+import HomePage from './pages/home';
+
+const Router = createStackNavigator(
+  {
+    Home: {
+      screen: HomePage
+    }
+  },
+  {
+    index: 0,
+    initialRouteName: 'Home',
+    /** No Header METHOD 1  */
+    headerMode: 'none',
+    /** No Header METHOD 2  */
+     defaultNavigationOptions: {
+       // header: () => null,
+    },
+  }
+);
+
+const AppContainer = createAppContainer(Router);
+
+export default (props) => {
+  return (
+    <View style={styles.flex}>
+      <Provider store={Store}>
+        <AppContainer screenProps={props} />
+      </Provider>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+    backgroundColor:'red'
+  }
+})
